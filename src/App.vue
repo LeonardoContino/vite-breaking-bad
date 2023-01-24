a1658b10c18042f3a5710d97740cd74a037f4b44
LeonardoContino <leoconti2000@hotmail.it>
2023-01-24 16:13:24 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script>
+import AppMain from './components/AppMain.vue';
+
+export default{
+  components:{AppMain}
+}
+</script>
+
+<template>
+  <body class="bg-danger">
+    <header class="d-flex justify-content-center p-4 ">
+    <img  src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/98/International_Pok%C3%A9mon_logo.svg/1200px-International_Pok%C3%A9mon_logo.svg.png" alt="pokemon">
+  </header>
+  <AppMain></AppMain>
+  </body>
+ 
+</template>
+
+<style scoped lang="scss">
+@use './assets/scss/style.scss' as *;
+body{
+  height: 100vh;
+}
+header img{
+  width: 200px;
+  text-align: center;
+}
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,32 @@
+<script>
+export default{
+
+}
+</script>
+<template>
+<section>
+    <h3 class="text-center p-2 text-warning">POKEDEX</h3>
+</section>
+<section class=" row row-cols-4 g-4 border pokedex">
+    <div class="col border"></div>
+    <div class="col border"></div>
+    <div class="col border"></div>
+    <div class="col border"></div>
+
+    
+
+</section>
+
+</template>
+<style scoped lang="scss">
+.pokedex{
+    min-height: 600px;
+    width: 1200px;
+    margin: 0 auto;
+    .col{
+        
+
+        
+    }
+}
+</style>
