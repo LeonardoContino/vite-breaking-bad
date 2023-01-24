bd43ab2d2fbcc8fc9f67fce463e010e49b8006fa
LeonardoContino <leoconti2000@hotmail.it>
2023-01-24 16:36:37 +0100

    aggiunto api

--- src/components/AppDataList.vue
+++ src/components/AppDataList.vue
@@ -1,6 +1,18 @@
 <script>
+import axios from 'axios'
 export default{
-    name: 'DataList'
+    name: 'DataList',
+    data(){
+        return{
+            ListPokemon:[]
+        }
+    },
+    created(){
+        axios.get('https://41tyokboji.execute-api.eu-central-1.amazonaws.com/dev/api/v1/pokemons?per=10&page=1')
+        .then(res =>{
+           this.ListPokemon = res.data.docs
+        })
+    }
 }
 </script>
 
